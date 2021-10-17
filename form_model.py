7f2ec7ecfcdf5139b52b2f8b7dc9f7d8d2834b04
subhadarship <subhadarshipanda08@gmail.com>
2021-10-17 03:07:20 -0400

    minor edits

--- form_model.py
+++ form_model.py
@@ -2,5 +2,7 @@ from wtforms import Form, validators, StringField, IntegerField
 
 
 class InputFormWord2Vec(Form):
+    """Input form class"""
     words = StringField(label='Odia words', default="ଗଛ, ସଙ୍ଗୀତ, ଚଳଚ୍ଚିତ୍ର", validators=[validators.InputRequired()])
-    num_neighbours = IntegerField(label='Number of neighbours', default=5, validators=[validators.InputRequired()])
+    num_neighbours = IntegerField(label='Number of neighbours for each word', default=5,
+                                  validators=[validators.InputRequired()])
